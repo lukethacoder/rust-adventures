1c5f4d90557d4a76986f59d242a9af7e4dca37d6
luke-secomb <lukessecomb@gmail.com>
2019-12-29 15:15:21 +1100

    rustlings variables/functions/enums/structs/test

---
+++ rustlings/exercises/enums/enums1.rs
@@ -0,0 +1,18 @@
+// enums1.rs
+// Make me compile! Execute `rustlings hint enums1` for hints!
+
+#[derive(Debug)]
+enum Message {
+    // TODO: define a few types of messages as used below
+    Quit,
+    Echo(String),
+    Move { x: i32, y: i32 },
+    ChangeColor(i32, i32, i32),
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo(String::from("echo me")));
+    println!("{:?}", Message::Move { x: 14, y: 15 });
+    println!("{:?}", Message::ChangeColor(25, 25, 25));
+}
