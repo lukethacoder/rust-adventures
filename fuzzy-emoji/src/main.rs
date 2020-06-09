8e04f72e193fccb8d6b27fd957ad3d3b32a6cff3
Luke Secomb <luke.secomb@live.com>
2020-06-09 21:33:55 +1000

    🎸 load in emoji.json / serialise to typed hashmap / example get value by string key

--- fuzzy-emoji/src/main.rs
+++ fuzzy-emoji/src/main.rs
@@ -1,25 +1,65 @@
-use fuzzy_matcher::skim::SkimMatcherV2;
-use fuzzy_matcher::FuzzyMatcher;
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::fs::read_to_string;
+use std::hash::Hash;
+use std::path::Path;
+use std::str;
+use std::time::Instant;
 
-fn main() {
-    // let search_this = get_user_search();
+// use ngrammatic::{CorpusBuilder, Pad};
 
-    // let matcher = SkimMatcherV2::default();
-    // assert_eq!(None, matcher.fuzzy_match("abc", "abx"));
-    // assert!(matcher.fuzzy_match("axbycz", "abc").is_some());
-    // assert!(matcher.fuzzy_match("axbycz", "xyz").is_some());
-    // println!(
-    //     "matcher.fuzzy_match('axbycz', 'xyz').is_some() {:?}",
-    //     matcher.fuzzy_match("axbycz", "xyz").is_some()
-    // );
+use colored::*;
+use serde::Deserialize;
+use serde::Serialize;
 
-    // let (score, indices) = matcher.fuzzy_indices("axbycz", "abc").unwrap();
-    // assert_eq!(indices, [0, 2, 4]);
-
-    // println!("indices {:?}", indices);
-    // println!("score {:?}", score);
+#[derive(Serialize, Deserialize, Debug)]
+struct Emoji {
+    a: String,
+    b: String,
+    c: Vec<String>,
 }
 
-fn get_user_search() {
-    // TODO:
+// #[derive(Serialize, Deserialize, Debug)]
+// struct EmojiAll {
+//     #[serde(flatten)]
+//     inner: HashMap<String, Emoji>,
+// }
+
+type EmojiHash = HashMap<String, Emoji>;
+
+fn print_map<K: Debug + Eq + Hash, V: Debug>(map: &HashMap<K, V>) {
+    for (k, v) in map.iter() {
+        println!("-----");
+        println!("{:?}: {:?}", k, v);
+        println!("-----");
+    }
+}
+
+fn main() {
+    // Fetch the file
+    let json_file_path = Path::new("emojis.json");
+    let json_file_str = read_to_string(json_file_path).expect("file not found");
+    let start = Instant::now();
+
+    // deserialise to EmojiHash
+    let _deserialized_camera: EmojiHash =
+        serde_json::from_str(&json_file_str).expect("error while reading json");
+    let elapsed = start.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    let hash_keys = _deserialized_camera.keys();
+    // println!("hash_keys: {:?}", hash_keys);
+    // println!("emoji json config: {:?}", &_deserialized_camera);
+
+    let key_to_get: &str = "fire";
+
+    // print_map(&_deserialized_camera);
+    println!("attempting to find {} in our hashmap", &key_to_get.green());
+
+    let get_the_opp: Option<&Emoji> = _deserialized_camera.get(key_to_get);
+    println!(
+        "found value where key = {}: {:?}",
+        &key_to_get.red(),
+        &get_the_opp
+    );
 }
