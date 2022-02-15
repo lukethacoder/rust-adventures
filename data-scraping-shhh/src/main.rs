eb598038350f2a808a3df6a6e2bd2d49d6b459f0
Luke Secomb <luke.secomb@live.com>
2022-02-15 15:53:15 +1100

    🚧 initial commit of data scraping

---
+++ data-scraping-shhh/src/main.rs
@@ -0,0 +1,77 @@
+use colored::*;
+use std::fs;
+use std::time::{Instant};
+
+pub mod types;
+
+const MAX_API_HITS: i32 = 150;
+const OFFSET_VALUE: i32 = 2;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let now = Instant::now();
+  let mut total_api_hits: i32 = 1;
+  let mut results: Vec<types::Result> = vec![];
+
+  let data = get_data(6).await?;
+
+  // println!("Total count of {}", &data.count.to_string().red());
+  // println!("should hit the API {} times.\n", &data.count / 20);
+
+  // results.extend_from_slice(&data.results);
+
+  // for num in 0..MAX_API_HITS {
+  //   // change it to get range
+  //   println!("Fetching with offset {}", num + OFFSET_VALUE);
+  //   // hit the API here
+  //   let response_data = get_data(num + OFFSET_VALUE).await?;
+  //   results.extend_from_slice(&response_data.results);
+  //   total_api_hits = total_api_hits + 1;
+  // }
+  // println!(
+  //   "Saving a total of {} items to the json file.",
+  //   results.len().to_string().green()
+  // );
+
+  // let as_string = serde_json::to_string_pretty(&results).unwrap();
+  // fs::write("zeengo.json", as_string).expect("Unable to write file");
+
+  // println!(
+  //   "Hit the API {} times, and took {}ms",
+  //   &total_api_hits.to_string().green(),
+  //   now.elapsed().as_millis().to_string().red()
+  // );
+  Ok(())
+}
+
+pub async fn get_data(
+  page_number: i32,
+) -> Result<types::ResponseObject, Box<dyn std::error::Error>> {
+  println!("{}", "Hitting endpoint to fetch some data".magenta());
+
+  let endpoint_url = format!(
+    "https://zango.com.au/api/properties/?page={page_number}",
+    page_number = page_number,
+  );
+
+  let client = reqwest::Client::new();
+  let response = client
+    .get(endpoint_url)
+    .header("Content-Type", "application/json")
+    .send()
+    .await?;
+  // eprintln!("Response: {:?} {}", response.version(), response.status());
+  // eprintln!("Headers: {:#?}\n", response.headers());
+
+  println!(
+    "?page={} - {}{} \n",
+    &page_number,
+    "Status ".blue(),
+    response.status().as_u16()
+  );
+  let json = response.json::<types::ResponseObject>().await?;
+  let json_as_string = serde_json::to_string(&json).unwrap();
+  let response_object = serde_json::from_str(&json_as_string).unwrap();
+
+  Ok(response_object)
+}
---
+++ data-scraping-shhh/src/types.rs
@@ -0,0 +1,283 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct ResponseObject {
+  pub count: i64,
+  pub results: Vec<Result>,
+  pub links: Links,
+  pub search_description: SearchDescription,
+  pub listing_type: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Links {
+  next: String,
+  previous: Option<serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Result {
+  id: i64,
+  status: Status,
+  property_class: PropertyClass,
+  listing_type: ListingType,
+  created: String,
+  updated: String,
+  #[serde(rename = "priceView")]
+  price_view: String,
+  address_postcode: String,
+  address_suburb: String,
+  address_suburb_display: bool,
+  address_state: State,
+  address_region: String,
+  location: Option<Location>,
+  headline: String,
+  bedrooms: i64,
+  bathrooms: i64,
+  parking: i64,
+  price: i64,
+  price_display: bool,
+  #[serde(rename = "underOffer")]
+  under_offer: bool,
+  authority: Authority,
+  auction_date: Option<String>,
+  #[serde(rename = "auctionVenue")]
+  auction_venue: Option<String>,
+  rent: Option<serde_json::Value>,
+  rent_period: RentPeriod,
+  #[serde(rename = "dateAvailable")]
+  date_available: Option<String>,
+  #[serde(rename = "commercialRent")]
+  commercial_rent: i64,
+  #[serde(rename = "commercialRent_period")]
+  commercial_rent_period: CommercialRentPeriod,
+  #[serde(rename = "carSpaces")]
+  car_spaces: i64,
+  #[serde(rename = "parkingComments")]
+  parking_comments: String,
+  area: Option<String>,
+  area_unit: AreaUnit,
+  address_display_string: String,
+  address_street_string: String,
+  images: Vec<ImageElement>,
+  agents: Vec<Agent>,
+  office: Office,
+  url_path: String,
+  category: String,
+  slug: String,
+  inspections: Vec<Inspection>,
+  #[serde(rename = "buildingDetails_energyRating")]
+  building_details_energy_rating: Option<String>,
+  last_price_change: Option<String>,
+  development: Option<serde_json::Value>,
+  #[serde(rename = "soldDetails_price")]
+  sold_details_price: Option<serde_json::Value>,
+  #[serde(rename = "soldDetails_date")]
+  sold_details_date: Option<serde_json::Value>,
+  #[serde(rename = "leasedDetails_price")]
+  leased_details_price: Option<serde_json::Value>,
+  stamp_duty: Option<String>,
+  development_show_full_result: bool,
+  has_live_auction: bool,
+  has_virtual_tour: bool,
+  first_go_live: String,
+  has_new_price: bool,
+  body_corporate_fees: String,
+  suburb_aliases: Vec<String>,
+  is_private_listing: bool,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Agent {
+  id: i64,
+  title: String,
+  name_short: String,
+  job_title: String,
+  results_image: Option<String>,
+  profile_image: Option<String>,
+  phone: String,
+  office_phone: String,
+  slug: String,
+  live: bool,
+  private_lister: bool,
+  rei_membership: Option<ReiMembership>,
+  rei_member_number: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct ImageElement {
+  image: ImageImage,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct ImageImage {
+  image_320_240: String,
+  image_800_600: String,
+  image_1200_680: String,
+  image_480_270: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Inspection {
+  inspection_time: String,
+  inspection_end_time: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Location {
+  #[serde(rename = "type")]
+  location_type: Type,
+  coordinates: Vec<f64>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Office {
+  title: String,
+  office_name: String,
+  logo: String,
+  logo_medium: String,
+  branding_colour: String,
+  branding_font_colour: String,
+  slug: String,
+  address: String,
+  suburb: String,
+  postcode: String,
+  state: State,
+  id: i64,
+  #[serde(rename = "show_1form")]
+  show_1_form: bool,
+  #[serde(rename = "show_1form_url")]
+  show_1_form_url: Option<serde_json::Value>,
+  sorted_services_id: String,
+  inspect_real_estate_id: String,
+  inspect_real_estate_sale_id: String,
+  inspect_real_estate_apply_id: String,
+  snug_office_id: SnugOfficeId,
+  snug_apply_now_enabled: bool,
+  snug_book_inspections_enabled_sale: bool,
+  snug_book_inspections_enabled_rent: bool,
+  payment_terms: i64,
+  calculator_provider_name: CalculatorProviderName,
+  calculator_provider_image: Option<String>,
+  calculator_provider_url: String,
+  phone: String,
+  live: bool,
+  website_url: String,
+  facebook_url: String,
+  linkedin_url: String,
+  instagram_url: String,
+  twitter_url: String,
+  youtube_url: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct SearchDescription {
+  search: String,
+  locations: String,
+  sold_leased_search: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum State {
+  #[serde(rename = "ACT")]
+  Act,
+  #[serde(rename = "NSW")]
+  Nsw,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum ReiMembership {
+  #[serde(rename = "")]
+  Empty,
+  #[serde(rename = "REIACT")]
+  Reiact,
+  #[serde(rename = "REINSW")]
+  Reinsw,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum AreaUnit {
+  #[serde(rename = "acre")]
+  Acre,
+  #[serde(rename = "hectare")]
+  Hectare,
+  #[serde(rename = "")]
+  Empty,
+  #[serde(rename = "squareMeter")]
+  SquareMeter,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum Authority {
+  #[serde(rename = "auction")]
+  Auction,
+  #[serde(rename = "exclusive")]
+  Exclusive,
+  #[serde(rename = "sale")]
+  Sale,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum CommercialRentPeriod {
+  #[serde(rename = "annual")]
+  Annual,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum ListingType {
+  #[serde(rename = "lease")]
+  Lease,
+  #[serde(rename = "sale")]
+  Sale,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum Type {
+  Point,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum CalculatorProviderName {
+  #[serde(rename = "")]
+  Empty,
+  #[serde(rename = "Loan Market Horizon")]
+  LoanMarketHorizon,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum SnugOfficeId {
+  #[serde(rename = "AY82HEKB")]
+  Ay82Hekb,
+  #[serde(rename = "")]
+  Empty,
+  #[serde(rename = "6ILJ899W")]
+  The6Ilj899W,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum PropertyClass {
+  #[serde(rename = "commercial")]
+  Commercial,
+  #[serde(rename = "land")]
+  Land,
+  #[serde(rename = "rental")]
+  Rental,
+  #[serde(rename = "residential")]
+  Residential,
+  #[serde(rename = "rural")]
+  Rural,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum RentPeriod {
+  #[serde(rename = "week")]
+  Week,
+  #[serde(rename = "weekly")]
+  Weekly,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum Status {
+  #[serde(rename = "current")]
+  Current,
+}
